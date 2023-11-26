5f03994caac4fa75c17a4d597a8f5b0ebc17d6b4
Wayne George <wayne@lucidpartnership.co.uk>
2023-11-26 20:01:51 +0000

    Initial Commit

---
+++ FeelingCards/FeelingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeelingCardsApp.swift
+//  FeelingCards
+//
+//  Created by Wayne George on 26/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct FeelingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
